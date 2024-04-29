473b22131e158d1b46363e937a16d013239ae815
SriShan01 <srisarma94@gmail.com>
2024-04-29 12:21:12 +0530

    first commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { TextField, Button, FormControl, InputLabel, Select, MenuItem, IconButton, InputAdornment, Typography } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { login } from './authActions'; // Importing login action creator directly
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      branch: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required('Required'),
+      password: Yup.string().required('Required'),
+      branch: Yup.string().required('Required'),
+    }),
+    onSubmit: values => {
+      login(values); // Directly calling login action creator
+    },
+  });
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white shadow-md rounded px-8 pt-8 pb-12 w-96">
+        <Typography variant="h5" align="center" gutterBottom>
+          Login
+        </Typography>
+        <form onSubmit={formik.handleSubmit} className="space-y-4">
+          <FormControl fullWidth variant="outlined">
+            <InputLabel htmlFor="username">User Name</InputLabel>
+            <TextField
+              id="username"
+              name="username"
+              label="User Name"
+              variant="outlined"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
+            />
+          </FormControl>
+          <FormControl fullWidth variant="outlined">
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              variant="outlined"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </FormControl>
+          <FormControl fullWidth variant="outlined">
+            <InputLabel htmlFor="branch">Branch</InputLabel>
+            <Select
+              id="branch"
+              name="branch"
+              value={formik.values.branch}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.branch && Boolean(formik.errors.branch)}
+            >
+              <MenuItem value="">Select Branch</MenuItem>
+              {[...Array(10)].map((_, index) => (
+                <MenuItem key={index} value={`Branch ${index + 1}`}>Branch {index + 1}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Button fullWidth variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
