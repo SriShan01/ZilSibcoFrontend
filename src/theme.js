ed18627babb4f698dda6dd793c96e2d0cef16f95
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2024-05-03 04:57:53 +0530

    initial configurations done

--- src/theme.js
+++ src/theme.js
@@ -2,71 +2,30 @@ export const colorTokens = {
   grey: {
     0: "#FFFFFF",
     10: "#F6F6F6",
-    50: "#F0F0F0",
-    100: "#E0E0E0",
-    200: "#C2C2C2",
-    300: "#A3A3A3",
-    400: "#858585",
-    500: "#666666",
-    600: "#4D4D4D",
-    700: "#333333",
-    800: "#1A1A1A",
-    900: "#0A0A0A",
-    1000: "#000000",
   },
   primary: {
     50: "#F0F0F0",
     100: "#FF6E00",
-    200: "#FF6E00",
-    300: "#FF6E00",
-    400: "#FF6E00",
-    500: "#FF6E00",
-    600: "#FF6E00",
-    700: "#FF6E00",
-    800: "#FF6E00",
-    900: "#FF6E00",
   },
 };
 
 export const themeSettings = (mode) => {
   return {
     palette: {
-      mode: mode,
-      ...(mode === "dark"
-        ? {
-    
-            primary: {
-              main: colorTokens.primary[500],
-              light: colorTokens.primary[800],
-            },
-            neutral: {
-              main: colorTokens.grey[200],
-              mediumMain: colorTokens.grey[300],
-              medium: colorTokens.grey[400],
-              light: colorTokens.grey[700],
-            },
-            background: {
-              default: colorTokens.grey[900],
-              alt: colorTokens.grey[800],
-            },
-          }
-        : {
-        
-            primary: {
-              main: colorTokens.primary[500],
-              light: colorTokens.primary[50],
-            },
-            neutral: {
-              main: colorTokens.grey[500],
-              mediumMain: colorTokens.grey[400],
-              medium: colorTokens.grey[300],
-              light: colorTokens.grey[50],
-            },
-            background: {
-              default: colorTokens.grey[10],
-              alt: colorTokens.grey[0],
-            },
-          }),
+      primary: {
+        main: colorTokens.primary[500],
+        light: colorTokens.primary[50],
+      },
+      neutral: {
+        main: colorTokens.grey[500],
+        mediumMain: colorTokens.grey[400],
+        medium: colorTokens.grey[300],
+        light: colorTokens.grey[50],
+      },
+      background: {
+        default: colorTokens.grey[10],
+        alt: colorTokens.grey[0],
+      },
     },
     typography: {
       fontFamily: ["Rubik", "sans-serif"].join(","),
