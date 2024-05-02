92192c95fb288db74b65ea8b3a92fdf36a4c7576
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2024-05-03 04:56:13 +0530

    initial configurations done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 // import { useState } from 'react'
-import './App.css';
-// import LoginForm from './components/LoginForm';
-// import SideBar from './components/SideBar';
-// import SearchBar from './components/SearchBar';
-// import EditButton from './components/EditButton';
-// import SaveButton from './components/SaveButton';
-// import Pagination from './components/Pagination';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import SideBar from "./components/SideBar";
+import SearchBar from "./components/SearchBar";
+import EditButton from "./components/EditButton";
+import SaveButton from "./components/SaveButton";
+import Pagination from "./components/Pagination";
 
 function App() {
-
   return (
-    <>
-    hi
-    </>
-  )
+    <div>
+      <ToastContainer autoClose={2000} theme="dark" />
+      <Router>
+        <Routes>
+          <Pagination />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
